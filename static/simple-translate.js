afd0e5a0e21910b0ee85befe3f82271e030728eb
pkbsa <siranut.poko@hotmail.com>
2023-10-31 02:39:05 +0700

    fixed microphone function

--- static/simple-translate.js
+++ static/simple-translate.js
@@ -236,34 +236,47 @@ if ("speechSynthesis" in window) {
   console.log("Speech synthesis is not supported in this browser.");
 }
 const microphoneButton = document.getElementById("microphone");
+let isMicrophoneActive = false; // Flag to track microphone status
+let recognition = null; // Store the recognition instance
 
 microphoneButton.addEventListener("click", function () {
-  const micElem = document.getElementById("microphone");
-  const select = document.getElementById("source_lang");
-  const selectedOption = select.options[select.selectedIndex];
-  const lang = selectedOption.value; // Language code
-
-  micElem.innerHTML =
-    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-fill" viewBox="0 0 16 16"><path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z"/><path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/></svg>';
-
-  const recognition = new webkitSpeechRecognition();
-  recognition.lang = lang; // Set the language for recognition
-
-  recognition.onresult = function (event) {
-    const transcript = event.results[0][0].transcript;
-    const inputText = $("#input_text");
-
-    // Append the recognized speech to the input_text field
-    inputText.val(inputText.val() + " " + transcript);
-
+  console.log(isMicrophoneActive)
+  if (isMicrophoneActive) {
+    // Stop the microphone if it's active
     recognition.stop();
-    translateText();
-    micElem.innerHTML =
+    isMicrophoneActive = false;
+    microphoneButton.innerHTML =
       '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-mute-fill" viewBox="0 0 16 16"><path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4.02 4.02 0 0 0 12 8V7a.5.5 0 0 1 1 0v1zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a4.973 4.973 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4zm3-9v4.879L5.158 2.037A3.001 3.001 0 0 1 11 3z"/><path d="M9.486 10.607 5 6.12V8a3 3 0 0 0 4.486 2.607zm-7.84-9.253 12 12 .708-.708-12-12-.708.708z"/></svg>';
-  };
+  } else {
+    // Start the microphone
+    const micElem = document.getElementById("microphone");
+    const select = document.getElementById("source_lang");
+    const selectedOption = select.options[select.selectedIndex];
+    const lang = selectedOption.value; // Language code
 
-  recognition.start();
+    micElem.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-fill" viewBox="0 0 16 16"><path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z"/><path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/></svg>';
+
+    recognition = new webkitSpeechRecognition();
+    recognition.lang = lang; // Set the language for recognition
+
+    recognition.onresult = function (event) {
+      const transcript = event.results[0][0].transcript;
+      const inputText = $("#input_text");
+
+      // Append the recognized speech to the input_text field
+      inputText.val(inputText.val() + " " + transcript);
+
+      recognition.stop();
+      micElem.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-mute-fill" viewBox="0 0 16 16"><path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4.02 4.02 0 0 0 12 8V7a.5.5 0 0 1 1 0v1zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a4.973 4.973 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4zm3-9v4.879L5.158 2.037A3.001 3.001 0 0 1 11 3z"/><path d="M9.486 10.607 5 6.12V8a3 3 0 0 0 4.486 2.607zm-7.84-9.253 12 12 .708-.708-12-12-.708.708z"/></svg>';
+    };
+
+    recognition.start();
+    isMicrophoneActive = true;
+  }
 });
+
 
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
